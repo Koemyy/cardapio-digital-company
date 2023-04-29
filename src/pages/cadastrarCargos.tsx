ab6226be2a5367ddddd09c65e1e39f9d61ba5fcd
Koemy <francine.pns@gmail.com>
2023-04-29 16:58:45 -0300

    Instalei a biblioteca de icones phosphor

---
+++ src/pages/cadastrarCargos.tsx
@@ -0,0 +1,9 @@
+function CadastrarCargos() {
+    return (
+        <div className=" mx-5 my-4">
+<h1 className="text-3xl text-blue-700">Permissões</h1>
+        </div>
+    )
+}
+
+export default CadastrarCargos
