8aa3115145934692b1dbf5fee3ea9222abc244ff
Koemy <francine.pns@gmail.com>
2023-05-15 10:25:53 -0300

    Tela de cadastro de colaborador criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // import CadastrarCargos from "./pages/cadastrarCargos";
-import Cargos from "./pages/cargos.tsx";
 import HeaderEmpresa from "./components/HeaderEmpresa.tsx";
+import CadastrarColaborador from "./pages/cadastrarColaborador.tsx";
 export function App() {
 
   return (
       <div>
         <HeaderEmpresa/>
-        <Cargos/>
+        <CadastrarColaborador/>
       </div>
   )
 }
