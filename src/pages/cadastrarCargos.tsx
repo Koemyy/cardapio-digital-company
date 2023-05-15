4b42df71452ab8e73e3e244a4998b19c8720aba2
Koemy <francine.pns@gmail.com>
2023-05-15 10:26:15 -0300

    Adicionei um input para escrever o nome do cargo na rota cadastrarCargos

--- src/pages/cadastrarCargos.tsx
+++ src/pages/cadastrarCargos.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import Notification from "../components/Notification.tsx";
 import {CheckCircle} from '@phosphor-icons/react';
+
 function CadastrarCargos() {
     const [showNotification, setNotification] = useState(false);
 
@@ -15,9 +16,12 @@ function CadastrarCargos() {
     return (
         <div className="text-white-300 mx-5 my-4">
             <h1 className="text-3xl mb-4">Permissões</h1>
-            <p className="text-lg font-bold">Garçom</p>
-            <p className="py-4 font-extralight">Para conceder uma permissão clique no checkbox ao lado da opção
+            <p className="pb-4 font-extralight">Para conceder uma permissão clique no checkbox ao lado da opção
                 desejada.</p>
+            <form action="">
+                <label form="nome-cargo">Nome do cargo: </label>
+                <input name="nome-cargo" id="nome-cargo" className="mb-2 text-black-500 text-lg rounded"/>
+            </form>
             <div className="block h-[1px] border-0 border-t border-solid border-grey-300 mt-1 p-0"></div>
             <div className="py-4">
                 <div className="pb-4">
@@ -35,7 +39,9 @@ function CadastrarCargos() {
                 <div className="block h-[1px] border-0 border-t border-solid border-grey-300 mt-1 mt-4 p-0"></div>
             </div>
             <div className="flex items-center content-center justify-center pt-4">
-                <button onClick={notificationPopUp} className="hover:bg-black-400 hover:text-white-300 bg-white-300 px-4 py-4 rounded-3xl text-black-500 font-bold">Cadastrar</button>
+                <button onClick={notificationPopUp}
+                        className="hover:bg-black-400 hover:text-white-300 bg-white-300 px-4 py-4 rounded-3xl text-black-500 font-bold">Cadastrar
+                </button>
             </div>
             {showNotification && (
                 <Notification
