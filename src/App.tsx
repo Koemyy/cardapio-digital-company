e302a5fbe80393132232fda82e4a1f590518dc31
Koemy <francine.pns@gmail.com>
2023-05-27 15:32:22 -0300

    Criei as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-// import CadastrarCargos from "./pages/cadastrarCargos";
 import HeaderEmpresa from "./components/HeaderEmpresa.tsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/home.tsx";
 import CadastrarColaborador from "./pages/cadastrarColaborador.tsx";
+import CadastrarCargos from "./pages/cadastrarCargos";
+import Cargos from "./pages/cargos.tsx";
 export function App() {
 
-  return (
-      <div>
-        <HeaderEmpresa/>
-        <CadastrarColaborador/>
-      </div>
-  )
+    return (
+        <BrowserRouter>
+            <HeaderEmpresa/>
+            <Routes>
+                <Route index element={<Home />} />
+                <Route path="cadastrar-colaborador" element={<CadastrarColaborador/>}></Route>
+                <Route path="cadastrar-cargos" element={<CadastrarCargos/>}></Route>
+                <Route path="cargos" element={<Cargos/>}></Route>
+                <Route path="cargos" element={<Cargos/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
