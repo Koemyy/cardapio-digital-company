5541fa2063fe08e14bc7fb0d156042804643d11d
Koemy <francine.pns@gmail.com>
2023-05-27 15:29:45 -0300

    Adicionei dropdown com cargos

--- src/pages/cadastrarColaborador.tsx
+++ src/pages/cadastrarColaborador.tsx
@@ -17,23 +17,36 @@ function CadastrarColaborador() {
     return (
         <div className="text-white-300 mx-5 my-4">
             <h1 className="text-3xl md:text-4xl mb-4">Cadastrar colaborador</h1>
-            <p className="pb-4 md:text-2xl font-extralight">Para realizar o cadastro de um colaborador, preencha as informações abaixo.</p>
+            <p className="pb-4 md:text-2xl font-extralight">Para realizar o cadastro de um colaborador, preencha as
+                informações abaixo.</p>
             <form className="" action="">
                 <label className="md:text-xl" form="nome-colaborador">Nome completo: </label>
                 <br/>
-                <input name="nome-colaborador" id="nome-colaborador" size={30} className="my-2 text-black-500 text-lg rounded"/>
+                <input name="nome-colaborador" id="nome-colaborador" size={30}
+                       className="my-2 text-black-500 text-lg rounded"/>
                 <br/>
                 <label className="md:text-xl" form="email-colaborador">E-mail: </label>
                 <br/>
-                <input type="email" name="email-colaborador" id="email-colaborador" size={30} className="my-2 text-black-500 text-lg rounded"/>
+                <input type="email" name="email-colaborador" id="email-colaborador" size={30}
+                       className="my-2 text-black-500 text-lg rounded"/>
                 <br/>
                 <label className="md:text-xl" form="senha-colaborador">Senha: </label>
                 <br/>
-                <input type="password" name="senha-colaborador" id="senha-colaborador" size={30} className="my-2 text-black-500 text-lg rounded"/>
+                <input type="password" name="senha-colaborador" id="senha-colaborador" size={30}
+                       className="my-2 text-black-500 text-lg rounded"/>
                 <br/>
                 <label className="md:text-xl" form="confirmarsenha-colaborador">Confirmar senha: </label>
                 <br/>
-                <input type="password" name="confirmarsenha-colaborador" id="confirmarsenha-colaborador" size={30} className="my-2 text-black-500 text-lg rounded"/>
+                <input type="password" name="confirmarsenha-colaborador" id="confirmarsenha-colaborador" size={30}
+                       className="my-2 text-black-500 text-lg rounded"/>
+                <label form="selectList">Escolha um cargo:</label>
+                <br/>
+                <select className="my-2 py-2 text-black-500 text-lg rounded" name="selectList" id="selectList">
+                    <option value="garcom">Garçom</option>
+                    <option value="chefe-de-cozinha">Chefe de cozinha</option>
+                    <option value="caixa">Caixa</option>
+                    <option value="gerente">Gerente</option>
+                </select>
             </form>
             <div className="flex items-center content-center justify-center pt-4">
                 <button onClick={notificationPopUp}
