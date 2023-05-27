a462af868b3e60a5d35ff41aeeafc5d644887310
Koemy <francine.pns@gmail.com>
2023-05-27 17:07:00 -0300

    Nova rota criada para a tela que exibe o qrcode

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Home from "./pages/home.tsx";
 import CadastrarColaborador from "./pages/cadastrarColaborador.tsx";
 import CadastrarCargos from "./pages/cadastrarCargos";
 import Cargos from "./pages/cargos.tsx";
+import Qrcode from "./pages/qrcode.tsx";
+import QRCodeImagem from "./components/QRCodeImagem.tsx";
 export function App() {
 
     return (
@@ -14,7 +16,8 @@ export function App() {
                 <Route path="cadastrar-colaborador" element={<CadastrarColaborador/>}></Route>
                 <Route path="cadastrar-cargos" element={<CadastrarCargos/>}></Route>
                 <Route path="cargos" element={<Cargos/>}></Route>
-                <Route path="cargos" element={<Cargos/>}></Route>
+                <Route path="qrcode" element={<Qrcode/>}></Route>
+                <Route path="qrcode-gerado" element={<QRCodeImagem/>}></Route>
             </Routes>
         </BrowserRouter>
     )
