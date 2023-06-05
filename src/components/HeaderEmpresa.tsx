e8739da81dcb72707a7779938b19fd1b0d2ac7f5
Koemy <francine.pns@gmail.com>
2023-06-05 15:54:17 -0300

    Criei uma interface para poder modificar os atributos de acordo com a página.

--- src/components/HeaderEmpresa.tsx
+++ src/components/HeaderEmpresa.tsx
@@ -1,20 +1,21 @@
-import {ArrowCircleLeft} from '@phosphor-icons/react';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import {ReactElement} from "react";
 
-function HeaderEmpresa() {
+interface redirect {
+    to: string,
+    icon: ReactElement
+}
+function HeaderEmpresa({to, icon}: redirect) {
     return (
         <div>
-            <div className="flex">
-                <Link to="/home">
-                    <i className="text-orange-500 pl-4 pt-4 font-bold justify-center items-center"><ArrowCircleLeft
-                        size={42}/></i>
+            <div className="flex pl-2">
+                <Link to={to}><span className="text-orange-500 pl-4 pt-4 pr-10 font-bold justify-center items-center">{icon}</span>
                 </Link>
-                <h1 className="text-orange-500 pl-4 text-4xl pt-5 font-bold justify-center items-center">Rockland
-                    Bar</h1>
+                <h1 className="text-orange-500 pl-4 text-4xl pt-5 font-bold justify-center items-center">Rockland Bar</h1>
             </div>
             <p className="text-white-300 ml-[3em] text-xl justify-center items-center font-bold">Bem-vindo!</p>
         </div>
-    )
+    );
 }
 
 export default HeaderEmpresa;
