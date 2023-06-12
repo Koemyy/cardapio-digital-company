0b80f6e29203eacb5ba3e116ad44a8666af59f9b
Koemy <francine.pns@gmail.com>
2023-06-12 10:55:42 -0300

    Criei a tela pedidosGarcom

--- src/pages/pedidosGarcom.tsx
+++ src/pages/pedidosGarcom.tsx
@@ -1,31 +1,29 @@
-import { ArrowCircleLeft } from '@phosphor-icons/react';
-import HeaderEmpresa from '../components/HeaderEmpresa.tsx';
 import CardPedidos from '../components/cardPedidos.tsx';
+import { ArrowCircleLeft } from "@phosphor-icons/react";
+import HeaderEmpresa from "../components/HeaderEmpresa.tsx";
 
-function PedidosFinalizados({ pedidosFinalizados }: any) {
+function PedidosGarcom({ pedidosFinalizados, exibirHeader }: any) {
+    if (!pedidosFinalizados || pedidosFinalizados.length === 0) {
+        return <div className="text-white-300 text-xl flex justify-center align-middle items-center">Nenhum pedido finalizado encontrado.</div>;
+    }
+
     return (
         <div>
-            <HeaderEmpresa icon={<ArrowCircleLeft size={42} />} to="/home" />
-            <div className="mx-5 my-4">
-                <div className="flex">
-                    <div className="w-1/2 right-0">
-                        <div className="text-3xl font-bold text-white-300">Finalizados</div>
-                        <div className="opacity-25">
-                            {pedidosFinalizados.map((pedido: any, index: number) => (
-                                <CardPedidos
-                                    key={index}
-                                    mesa={pedido.mesa}
-                                    idPedido={pedido.idPedido}
-                                    pedidos={pedido.pedidos}
-                                    isFinalizado={true}
-                                />
-                            ))}
-                        </div>
-                    </div>
-                </div>
+            {exibirHeader && <HeaderEmpresa icon={<ArrowCircleLeft size={42} />} to="/home" />}
+            <div className="text-3xl font-bold text-white-300">Finalizados</div>
+            <div className="opacity-25">
+                {pedidosFinalizados.map((pedido: any, index: number) => (
+                    <CardPedidos
+                        key={index}
+                        mesa={pedido.mesa}
+                        idPedido={pedido.idPedido}
+                        pedidos={pedido.pedidos}
+                        isFinalizado={true}
+                    />
+                ))}
             </div>
         </div>
     );
 }
 
-export default PedidosFinalizados;
+export default PedidosGarcom;
