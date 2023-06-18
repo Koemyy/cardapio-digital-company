175d7e1c18ba9aa0dbf30604f9793ccad5c2418a
Koemy <francine.pns@gmail.com>
2023-06-18 18:18:04 -0300

    Atualizações utilizando novas props inseridas

--- src/pages/pedidosGarcom.tsx
+++ src/pages/pedidosGarcom.tsx
@@ -1,70 +1,85 @@
-import CardPedidos from '../components/cardPedidos.tsx';
-import { ArrowCircleLeft } from "@phosphor-icons/react";
+
+import {ArrowCircleLeft} from "@phosphor-icons/react";
 import HeaderEmpresa from "../components/HeaderEmpresa.tsx";
-import { useEffect, useState } from 'react';
-import { buscarPedidos } from '../service/PedidosService.tsx';
-
-
+import {useEffect, useState} from 'react';
+import {buscarPedidos, AtualizarStatusPedidos} from '../service/PedidosService.tsx';
+import CardPedidosGarcom from "../components/cardPedidosGarcom.tsx";
 
 interface Pedido {
-    ped_id : number
-    cli_id:  number
+    ped_id: number
+    cli_id: number
     pro_id: number
-    ped_status : string
-    ped_quantidade : number
+    ped_status: string
+    ped_quantidade: number
     pro_nome: string
-    mes_id : number
+    mes_id: number
 }
 
-function PedidosGarcom({exibirHeader}:any) {
-
+function PedidosGarcom() {
     const [pedidosProntos, setPedidosProntos] = useState<Pedido[][]>([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         async function fetchPedidosProntos() {
-            await buscarPedidos("P").then((data: Pedido[])=>{
-                const grouped: { [key: number]: Pedido[] } = data.reduce((result, item) => {
-                      const mesid = item.mes_id;
-                      if (!result[mesid]) {
-                        result[mesid] = [];
-                      }
-                      result[mesid].push(item);
-                     
-                      return result;
+            await buscarPedidos("P").then((data: Pedido[]) => {
+                const grouped: { [key: number]: Pedido[] } = data.reduce(
+                    (result, item) => {
+                        const mesid = item.mes_id;
+                        if (!result[mesid]) {
+                            result[mesid] = [];
+                        }
+                        result[mesid].push(item);
+
+                        return result;
                     },
-                    {} as { [key: number]: Pedido[] } 
-                  );
-                  setPedidosProntos(Object.values(grouped));
+                    {} as { [key: number]: Pedido[] }
+                );
+                setPedidosProntos(Object.values(grouped));
             });
         }
-       
+
         fetchPedidosProntos();
 
-        const interval = setInterval(fetchPedidosProntos, 1000); 
+        const interval = setInterval(fetchPedidosProntos, 1000);
         return () => {
             clearInterval(interval);
         };
-   },[])
+    }, []);
+
+    //Tentativa de atualizar status do pedido e remover o card
+
+    const removeCard = (index: number) => {
+        const updatedPedidosProntos = [...pedidosProntos];
+        updatedPedidosProntos.splice(index, 1);
+        setPedidosProntos(updatedPedidosProntos);
+    };
+
+    const updateStatus = async (index: number, updatedPedidos: string) => {
+        await AtualizarStatusPedidos(updatedPedidos, index);
+        removeCard(index);
+    };
 
     if (!pedidosProntos || pedidosProntos.length === 0) {
-        return <div className="text-white-300 text-xl flex justify-center align-middle items-center">Nenhum pedido finalizado encontrado.</div>;
+        return (
+            <div className="text-white-300 text-xl flex justify-center align-middle items-center">
+                Nenhum pedido finalizado encontrado.
+            </div>
+        );
     }
-
     return (
         <div>
-            {exibirHeader && <HeaderEmpresa icon={<ArrowCircleLeft size={42} />} to="/home" />}
+            <HeaderEmpresa icon={<ArrowCircleLeft size={42} />} to="/home" />
             <div className="text-3xl font-bold text-white-300">Finalizados</div>
-            <div className="opacity-25">
-                {pedidosProntos.map((pedido: any, index: number) => (
-                    <CardPedidos
+            {pedidosProntos.map((pedido: any, index: number) => (
+                <CardPedidosGarcom
                     idMesa={pedido[0].mes_id}
-                    key={index}
                     pedidos={pedido}
                     index={index}
                     isFinalizado={false}
-                    />
-                ))}
-            </div>
+                    exibirButton={true}
+                    exibirCheckbox={false}
+                    updateStatus={updateStatus}
+                />
+            ))}
         </div>
     );
 }
