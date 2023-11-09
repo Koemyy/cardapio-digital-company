10a1b48040e61a096d389421711fc58773313d26
Ueverson Ramos <ueversonrg@gmail.com>
2023-11-08 21:28:44 -0300

    corrigindo bug do status dos pedidos

--- src/service/PedidosService.tsx
+++ src/service/PedidosService.tsx
@@ -1,5 +1,3 @@
-
-
 interface Pedido {
     ped_id : number
     cli_id:  number
@@ -32,10 +30,53 @@ export async function buscarPedidos(status : string): Promise<Pedido[]>{
     return response;
 }
 
+export async function buscarPedidosParaPagamento(status : string): Promise<Pedido[]>{
+
+    const url = 'https://cardapio-digital-api.onrender.com/actions/buscarPedidosParaPagamento';
+   
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            status : status
+        })
+    })
+    .then((response)=>{
+        return response.json();
+    })
+    .catch((err) => err.message)
+
+    return response;
+}
+
 
 export async function AtualizarStatusPedidos(ped_status : string, ped_id : number): Promise<Pedido[]>{
 
     const url = 'https://cardapio-digital-api.onrender.com/actions/atualizarPedido';
+   
+    const response = await fetch(url,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            ped_status : ped_status,
+            ped_id: ped_id
+        })
+    })
+    .then((response)=>{
+        return response.json();
+    })
+    .catch((err) => err.message)
+
+    return response;
+}
+
+export async function AtualizarStatusPagamentoPedidos(ped_status : string, ped_id : number): Promise<Pedido[]>{
+
+    const url = 'https://cardapio-digital-api.onrender.com/actions/atualizarStatusPagementoPedido';
    
     const response = await fetch(url,{
         method: 'POST',
